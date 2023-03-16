0e1fe9dbb933a1c3b1a3d449877bb7043708750a
“spike” <2926169741@qq.com>
2023-03-16 16:08:54 +0800

    1.create project

--- Eureka_Spike/src/main/java/com/spike/EurekaServerApplication.java
+++ Eureka_Spike/src/main/java/com/spike/EurekaServerApplication.java
@@ -1,3 +1,5 @@
+package com.spike;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
