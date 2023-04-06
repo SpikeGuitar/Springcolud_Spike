348680d7c7d6cb38e781cfb9b7986f32c9edca8a
FOT-spike <2926169741@qq.com>
2023-04-06 10:30:05 +0800

    1.添加gdal图片裁剪方法 2.添加方法注释

--- Gis/src/main/java/com/spike/GisApplication.java
+++ Gis/src/main/java/com/spike/GisApplication.java
@@ -1,16 +1,20 @@
 package com.spike;
 
-import com.alibaba.fastjson.JSON;
-import com.spike.entity.GisPictureBase;
 import com.spike.utils.GdalUtils;
+import org.gdal.gdalconst.gdalconstConstants;
+import org.gdal.gdalconst.gdalconstJNI;
 
 public class GisApplication {
 
     public static void main(String[] args) {
         System.out.println("***************** 《开始执行测试程序》********************");
         GdalUtils gdalUtils = new GdalUtils();
-        GisPictureBase gisPictureBase = gdalUtils.printPictureInfoBySystemPath("D:\\DemoFile\\testTif.tiff");
-        System.out.println(JSON.toJSON(gisPictureBase));
+        gdalUtils.cutPicture("D:\\DemoFile\\testTIFF.tiff","D:\\DemoFile\\testTIFF4.tiff","GTiff",0,0,150,150);
+//        gdalUtils.printDriver();
+//        gdalUtils.driverByShortName("GTiff");
+//        GisPictureBase gisPictureBase = gdalUtils.cutPicture("D:\\DemoFile\\testTif.tiff");
+//        System.out.println(JSON.toJSON(gisPictureBase));
         System.out.println("***************** 《测试程序关闭》***********************");
+
     }
 }
--- Gis/src/main/java/com/spike/utils/GdalUtils.java
+++ Gis/src/main/java/com/spike/utils/GdalUtils.java
@@ -9,11 +9,15 @@ import org.gdal.gdal.Band;
 import org.gdal.gdal.Dataset;
 import org.gdal.gdal.Driver;
 import org.gdal.gdal.gdal;
+import org.gdal.gdalconst.gdalconst;
+import org.gdal.gdalconst.gdalconstConstants;
+import org.gdal.gdalconst.gdalconstJNI;
 import org.gdal.ogr.ogr;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GdalUtils {
 
@@ -25,16 +29,18 @@ public class GdalUtils {
         int count = ogr.GetDriverCount();
         for (int i = 0; i < count; i++) {
             org.gdal.ogr.Driver driver = ogr.GetDriver(i);
-            System.out.print( JSON.toJSON(driver).toString()+ "\n");
+            System.out.print(JSON.toJSON(driver).toString() + "\n");
         }
         gdal.GDALDestroyDriverManager();
     }
 
     /**
      * 按照输入的驱动简称 输出驱动的信息和全部方法
-     * @param shortName
+     *
+     * @param shortName 驱动简称
+     * @return 驱动数据信息
      */
-    public GisDriverBase printDriverByShortName(String shortName) {
+    public GisDriverBase driverByShortName(String shortName) {
         GisDriverBase gisDriverBase = new GisDriverBase();
         gdal.AllRegister();
         Driver driver = gdal.GetDriverByName(shortName);
@@ -45,18 +51,23 @@ public class GdalUtils {
         Class<?> methods = Driver.class;
         Method[] methodList = methods.getDeclaredMethods();
         List<String> list = new ArrayList<>();
-        for (Method obj :methodList) {
+        for (Method obj : methodList) {
             list.add(obj.getName());
         }
         gisDriverBase.setMethodList(list);
         return gisDriverBase;
     }
 
-    public GisPictureBase printPictureInfoBySystemPath(String systemPath) {
-        String srcFile = systemPath;
+    /**
+     * 获取目标图片数据信息
+     *
+     * @param systemPath 目标图片 系统路径
+     * @return 目标图片数据信息
+     */
+    public GisPictureBase pictureInfoBySystemPath(String systemPath) {
         gdal.AllRegister();
         GisPictureBase gisPictureBase = new GisPictureBase();
-        Dataset dataSet = gdal.Open(srcFile);
+        Dataset dataSet = gdal.Open(systemPath);
         gisPictureBase.setDataSet(dataSet);
         int width = dataSet.getRasterXSize();
         gisPictureBase.setWidth(width);
@@ -71,7 +82,7 @@ public class GdalUtils {
         String describe = dataSet.GetDescription();
         gisPictureBase.setDescribe(describe);
         List<Band> bandList = new ArrayList<>();
-        for (int i=1;i<=bandCount;i++) {
+        for (int i = 1; i <= bandCount; i++) {
             Band band = dataSet.GetRasterBand(i);
             bandList.add(band);
         }
@@ -79,4 +90,44 @@ public class GdalUtils {
         return gisPictureBase;
     }
 
+    /**
+     * 获取图像对图像进行裁剪
+     *
+     * @param oldSystemPath 目标图像系统路径
+     * @param currentSystemPath 生成图像系统路径
+     * @param shortName 驱动简称
+     * @param startX 水平位移像素
+     * @param startY 上下平移像素
+     * @param width 生成图像宽
+     * @param height 生成图像长
+     */
+    public void cutPicture(String oldSystemPath, String currentSystemPath, String shortName,int startX,int startY,int width,int height) {
+//        注册驱动
+        gdal.AllRegister();
+        Driver driver = gdal.GetDriverByName(shortName);
+//        获取数据集
+        Dataset oldDataSet = gdal.Open(oldSystemPath);
+//        获取波段
+        int bandCount = oldDataSet.getRasterCount();
+        Dataset currentDataset = driver.Create(currentSystemPath, width, height, bandCount, oldDataSet.GetRasterBand(1).GetRasterDataType());
+        currentDataset.SetProjection(oldDataSet.GetProjection());
+        currentDataset.SetGeoTransform(oldDataSet.GetGeoTransform());
+        //按band读取
+        for (int i = 0; i < height; i++) {
+//            按行读取
+            for (int j = 1; j <= bandCount; j++) {
+                Band orgBand = oldDataSet.GetRasterBand(j);
+                int[] cache = new int[width];
+//              从位置x开始，只读一行  注：gdalconstConstants类 在新版本gdal中已经无法使用 需要使用gdalconstJNI类代替
+                orgBand.ReadRaster(startX, startY + i, width,1,gdalconstJNI.GDT_Int32_get(), cache);
+                Band desBand = currentDataset.GetRasterBand(j);
+//              从左上角开始，只写一行
+                desBand.WriteRaster(0, i, width, 1,gdalconstJNI.GDT_Int32_get(), cache);
+                desBand.FlushCache();
+            }
+        }
+//释放资源
+        oldDataSet.delete();
+        currentDataset.delete();
+    }
 }
