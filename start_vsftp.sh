12cf78aa4b8c02bfa1a7c2322ddd80614ec0400f
spike <2926169741@qq.com>
2023-04-24 15:07:11 +0800

    1.添加docker 脚本

--- start_vsftp.sh
+++ start_vsftp.sh
@@ -1,2 +1,2 @@
 docker run -itd --privileged -e FTP_USER=vsftp -e FTP_PASS=656803  --name=vsftp -v /root/data:/home/vsftp -p 20:20 -p 21:21 -p 21100-21110:21100-21110   spikebob/vsftp:v1.0.2  /usr/sbin/init
-docker exec -it spike  /bin/bash
+docker exec -it vsftp  /bin/bash
